5a2d206a63143687a5b9bcce15b6d7edf914e341
sampaniony <sampanionyra55@gmail.com>
2023-10-20 06:04:16 +0300

    inscription en cours

--- components/auth/RegisterScreen.js
+++ components/auth/RegisterScreen.js
@@ -1,27 +1,55 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import jsonUsers from '../../assets/json/users.json';
+import * as FileSystem from 'expo-file-system'
 
 export default function RegisterScreen() {
+	const users = jsonUsers;
     const [utilisateur, setUtilisateur] = useState({
         name: '',
         email: '',
         username: '',
         password: '',
     });
+
     const handleChange = (text, champ) => {
         //ici
         setUtilisateur({...utilisateur, [champ]: text})
     };
 
-    const handleRegister = () => {
+    const handleRegister = async () => {
         // Vous pouvez utiliser l'objet utilisateur ici pour soumettre les données.
         console.log('Utilisateur enregistré :', utilisateur);
-      };
+        const jsonFilePath = `${FileSystem.documentDirectory}/users.json`; // Chemin de destination dans le répertoire de documents
+
+        if (jsonFilePath.exists) {
+            console.log("OK")
+        } else {
+            console.log('Le fichier n\'existe pas.');
+        }
+        // try {
+        //     const jsonFilePath = `${FileSystem.documentDirectory}/${users}`;
+
+        //     if (fileInfo.exists) {
+        //         console.log("OK")
+        //     } else {
+        //         console.log('Le fichier n\'existe pas.');
+        //     }
+        //     // const existingData = await FileSystem.readAsStringAsync(jsonFilePath);
+        //     // const parsedData = JSON.parse(existingData);
+
+        //     // console.log(parsedData);
+        // }
+        // catch (erreur) {
+        //     console.log('Erreur produite')
+        // }
+    };
     
     return (
-        <View>
+        <View style={styles.container}>
             <Text>Inscription</Text>
             <TextInput
+                style={styles.input}
                 placeholder='Nom'
                 value={utilisateur.name}
                 onChangeText={(text) => handleChange(text, 'name')}
@@ -29,6 +57,7 @@ export default function RegisterScreen() {
             </TextInput>
 
             <TextInput
+                style={styles.input}
                 placeholder='Email'
                 value={utilisateur.email}
                 onChangeText={(text) => handleChange(text, 'email')}
@@ -37,6 +66,7 @@ export default function RegisterScreen() {
             </TextInput>
 
             <TextInput
+                style={styles.input}
                 placeholder='Pseudo'
                 value={utilisateur.username}
                 onChangeText={(text) => handleChange(text, 'username')}
@@ -44,6 +74,7 @@ export default function RegisterScreen() {
             </TextInput>
 
             <TextInput
+                style={styles.input}
                 placeholder='Mot de passe'
                 value={utilisateur.password}
                 onChangeText={(text) => handleChange(text, 'password')}
@@ -56,3 +87,16 @@ export default function RegisterScreen() {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    input: {
+		height: 40,
+		borderColor: 'gray',
+		borderWidth: 1,
+		marginBottom: 12,
+		paddingHorizontal: 8,
+	},
+    container: {
+        padding: '2rem',
+    }
+})
